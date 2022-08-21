fd7d5c9b645c8d2b6983b8c0c2786f7b6998eb90
martinloretzzz <20306567+martinloretzzz@users.noreply.github.com>
2022-08-21 16:02:09 +0200

    Updates

---
+++ precache-manifest.cf250a2a71b5348c679325f08f1a64e1.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59cc25f25ac972a27118d6fdeb3e5589",
+    "url": "/cabana/12dc4f509a04bc6c8735.worker.js"
+  },
+  {
+    "revision": "c47fb89f944fc413937f1d857df6495a",
+    "url": "/cabana/12dc4f509a04bc6c8735.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "28744d8361e2aa87dc591da02d1e48e7",
+    "url": "/cabana/13e81a499e541c9b2ae9.worker.js"
+  },
+  {
+    "revision": "c47fb89f944fc413937f1d857df6495a",
+    "url": "/cabana/13e81a499e541c9b2ae9.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6890bb6894afed6dd6a857fe7099a4a",
+    "url": "/cabana/6104103bc5c97cb8539f.worker.js"
+  },
+  {
+    "revision": "654ec567f721f0aa3267bd06c14dfc36",
+    "url": "/cabana/6104103bc5c97cb8539f.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "7445c87f5ab6bfcda5e8e1b6e71c20dd",
+    "url": "/cabana/ed040febd1ce733cecf6.worker.js"
+  },
+  {
+    "revision": "c47fb89f944fc413937f1d857df6495a",
+    "url": "/cabana/ed040febd1ce733cecf6.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "a65ff34adbcd8bb24e8aa8899d685c56",
+    "url": "/cabana/index.html"
+  },
+  {
+    "revision": "bd1cb6e557adf072b2b0",
+    "url": "/cabana/static/css/main.3725dec1.chunk.css"
+  },
+  {
+    "revision": "a15ec613cb14f066f407",
+    "url": "/cabana/static/js/2.a2f3fb3d.chunk.js"
+  },
+  {
+    "revision": "826687e094ef7ce0c659dad472ba284f",
+    "url": "/cabana/static/js/2.a2f3fb3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd1cb6e557adf072b2b0",
+    "url": "/cabana/static/js/main.09c6f309.chunk.js"
+  },
+  {
+    "revision": "566671bb000325010475",
+    "url": "/cabana/static/js/runtime-main.8bbb7936.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/cabana/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/cabana/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/cabana/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/cabana/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/cabana/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "e5a08e76f61c8ef370034956eb5865e1",
+    "url": "/cabana/static/media/webusb-enable-experimental-features.e5a08e76.png"
+  },
+  {
+    "revision": "aceef6a08425cf752843a9dabd208c56",
+    "url": "/cabana/static/media/webusb-enable-webusb.aceef6a0.png"
+  }
+]);
